99ffb550c897d06b43cb282f70bf6b910e2f62d5
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2022-03-02 22:36:30 +0700

    commit

--- routes/api.php
+++ routes/api.php
@@ -62,3 +62,13 @@ Route::get('/data/whereSimple',[DataController::class, 'whereSimple']);
 // Route::prefix('paging')->group(function () {
 //     Route::get('/paging', [, '']); //127.0.0.1:8000/api/paging/paging
 // });
+
+use App\Http\Controllers\API\BarangController;
+
+Route::prefix('barang')->group(function () {
+    Route::post('/insert', [BarangController::class, 'insert'])->name('barang.insert');
+    Route::get('/all', [BarangController::class, 'all'])->name('barang.all');
+    Route::post('/update/{id}', [BarangController::class, 'update'])->name('barang.update');
+    Route::get('/delete/{id}', [BarangController::class, 'delete'])->name('barang.delete');
+    Route::get('/find/{id}', [BarangController::class, 'find'])->name('barang.find');
+});
